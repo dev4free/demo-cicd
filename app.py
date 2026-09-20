484a356ce032c4cd2f88bfb06f486a95d60bd29a
ctomasi <ctomasi4c@gmail.com>
2026-09-20 18:06:01 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(b"<h1>Hello Cloud! Il ciclo CI/CD funziona!</h1>")
+
+if __name__ == '__main__':
+    server = HTTPServer(('0.0.0.0', 8080), SimpleHandler)
+    print("Server attivo sulla porta 8080...")
+    server.serve_forever()
